dd5386f36d864da6fc36aee0235de647503803ce
Stellorion <osher5209@gmail.com>
2025-06-03 15:40:23 +0300

    13th try

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,16 +1,27 @@
-interface UserBase {
-  id: string;
-  email: string;
-  fullName: string;
-  userRole: string;
-}
+import NextAuth from "next-auth";
 
-declare module 'next-auth' {
+declare module "next-auth" {
+  interface User {
+    id: string;
+    email: string;
+    fullName: string;
+    userRole: string;
+  }
   interface Session {
-    user: UserBase;
+    user: {
+      id: string;
+      email: string;
+      fullName: string;
+      userRole: string;
+    };
   }
 }
 
-declare module 'next-auth/jwt' {
-  interface JWT extends UserBase {}
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    email: string;
+    fullName: string;
+    userRole: string;
+  }
 }
