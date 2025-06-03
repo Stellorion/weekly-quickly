52e9456fddf5501c69592057ca786d6ad165815f
Stellorion <osher5209@gmail.com>
2025-06-03 15:37:41 +0300

    12th try

--- auth.ts
+++ auth.ts
@@ -1,4 +1,5 @@
-import NextAuth, { NextAuthOptions } from 'next-auth/';
+import NextAuth from 'next-auth';
+import type { NextAuthOptions } from 'next-auth/';
 import Credentials from 'next-auth/providers/credentials';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { db } from '@/db/connect';
